0245c25b9057704aefec5e155ae83ad564f1f68c
mspae <mail@martinspencer.de>
2022-04-03 18:03:58 +0200

    fix tests

--- src/iteration-cache.test.ts
+++ src/iteration-cache.test.ts
@@ -4,18 +4,19 @@ test("iteration cache entry and retrieval", () => {
   const cache = new IterationCache();
   const iterationState1 = [{ symbol: "bar", params: {} }];
   const iterationState2 = [{ symbol: "baz", params: { foo: 1 } }];
-  cache.setIterationCacheEntry(123, 1, iterationState1);
-  cache.setIterationCacheEntry(123, 2, iterationState2);
-  cache.setIterationCacheEntry(123, 1, iterationState2);
-  cache.setIterationCacheEntry(123, 1, iterationState1);
-  const cachedIteration = cache.requestIteration(123, 1);
+  cache.setIterationCacheEntry(1, iterationState1);
+  cache.setIterationCacheEntry(2, iterationState2);
+  cache.setIterationCacheEntry(1, iterationState2);
+  cache.setIterationCacheEntry(1, iterationState1);
+  const cachedIteration = cache.requestIteration(1);
   expect(cachedIteration).toMatchObject(iterationState1);
 });
 
 test("iteration cache cleaning", () => {
   const cache = new IterationCache();
   const iterationState = [{ symbol: "bar", params: {} }];
+  cache.setIterationCacheEntry(1, iterationState);
   cache.cleanCache();
-  const cachedIteration = cache.requestIteration(123, 1);
+  const cachedIteration = cache.requestIteration(1);
   expect(cachedIteration).toBeNull();
 });
